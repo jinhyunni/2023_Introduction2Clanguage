743acb12e70a87494e34f8b1bc57ac4f1756585e
jinhyunni <pjhhhk@gmail.com>
2023-04-17 18:15:09 +0900

    second commit

---
+++ 20230320/ex2_CS_Allignment.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+	char name[10] = "Jinhyun";
+
+	printf("%s\n", "Hello");
+	printf("My name is %4s\n", name);
+	printf("\n");
+	//print specific chracter in array
+	printf("%c\n",name[3]);
+	printf("%-20s %-5s\n", "Nice to meet you", "Bye!");
+
+	return 0;
+}
