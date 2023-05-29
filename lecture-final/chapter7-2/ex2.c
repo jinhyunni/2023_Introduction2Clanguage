b715fcce1d6ac58821a851a164e2fd65532e19ea
jinhyunni <pjhhhk@gmail.com>
2023-05-29 14:28:17 +0900

    update on example 2

--- lecture-final/chapter7-2/ex2.c
+++ lecture-final/chapter7-2/ex2.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
-#define SIZE 10
-//짝수인 원소만 출력하기
+#include <limits.h>
 
+#define SIZE 10
+#define MAX INT_MAX
+#define MIN INT_MIN
 
 int main()
 {	
 	int i;	//배열의 위치(첨자변수)
 	int count=0; //배열의 내용 중에서 짝수의 개수와 위치를 선언
+	int max=MIN;	//최댓값을 저장할 변수
+	int min=MAX; 	//최솟값을 저장할 변
 
 	int num[SIZE];	//배열선언
 	int even[SIZE]; //짝수를 저장할 배열
 	
 
 	//배열에 원소 입력
-	for(i=0; i<5; i++)
+	for(i=0; i<SIZE; i++)
 	{
 		printf("%d 번째 원소를 입력하시오: ", i+1);
 		scanf("%d", num+i);
@@ -21,7 +25,7 @@ int main()
 	
 
 	//짝수인 원소를 짝수 배열에 따로 저장하기
-	for(i=0; i<5; i++)
+	for(i=0; i<SIZE; i++)
 	{
 		if(num[i]%2 == 0)
 		{
@@ -34,22 +38,45 @@ int main()
 		}
 			
 	}
+
+	//배열에서 최댓값 찾기
+	for(i=0; i<SIZE; i++)
+	{
+		if(num[i]>max)
+			max = num[i];
+	}
 	
+	//배열에서 최솟값 찾기
+	for(i=0; i<SIZE; i++)
+	{
+		if(num[i]<min)
+			min = num[i];
+	}
+	
+	//결과값을 출력
  	printf("입력 배열의 내용 출력하기\n");
- 	for(i=0; i<5; i++)
+
+ 	for(i=0; i<SIZE; i++)
  	{
  		printf("%d 번쩨 원소: %d\n", i+1, num[i]);
 	}
-	
+	printf("\n");
+
+
 	printf("출력 배열의 내용 출력하기\n");
+
 	for(i=0; i<count; i++)
 	{
 		printf("%d 번째 원소: %d\n", i+1, even[i]);
 	}
 
 	printf("짝수의 개수: %d\n", count);
+	printf("\n");
 
 	printf("응용: 입력 배열에서 원소의 최댓값, 최솟값 출력해보기!\n");
+	printf("\n");
+	printf("배열에서의 최댓값: %d\n", max);
+	printf("배열에서의 최솟값: %d\n", min);
 
 	return 0;
 }
